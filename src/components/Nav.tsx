dcbe90e08058d62467a1f5b7a9e317c24b7a5c67
kethnal <kaseyvaldez@hotmail.com>
2023-05-11 15:01:31 -0700

    refactor nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -12,55 +12,52 @@ function Nav() {
 
   function handleToggleMenu() {
     setNavOpen(!navOpen);
-  }
+  };
+
+  addEventListener("resize", () => {
+    setNavOpen(false);
+  });
+
+  const navItems = <>
+    <ul>
+      <li className="nav-item">
+        <a href="/stories">STORIES</a>
+      </li>
+      <li className="nav-item">
+        <a href="/features">FEATURES</a>
+      </li>
+      <li className="nav-item">
+        <a href="/pricing">PRICING</a>
+      </li>
+    </ul>
+    <button className="button button_black_border">GET AN INVITE</button>
+  </>;
 
   return (
-    <nav className="nav">
-      <div className="nav_main">
-        <Link to="/" className="logo">
-          <img src={logo} alt="logo and go to home" />
-        </Link>
-        {!isDesktop && <button
-          aria-expanded={navOpen ? "true" : "false"}
-          className="toggle-menu"
-          onClick={handleToggleMenu}
-        >
-          <img
-            src={navOpen ? close : menu}
-            alt={navOpen ? "close menu" : "open menu"}
-          />
-        </button>}
-      </div>
-      {isDesktop && <>
-        <ul>
-          <li className="nav-item">
-            <a href="/stories">STORIES</a>
-          </li>
-          <li className="nav-item">
-            <a href="/features">FEATURES</a>
-          </li>
-          <li className="nav-item">
-            <a href="/pricing">PRICING</a>
-          </li>
-        </ul>
-        <button className="button button_black_border">GET AN INVITE</button>
-      </>}
-      {navOpen && <div className="nav_list">
-        <ul>
-          <li className="nav-item">
-            <a href="/stories">STORIES</a>
-          </li>
-          <li className="nav-item">
-            <a href="/features">FEATURES</a>
-          </li>
-          <li className="nav-item">
-            <a href="/pricing">PRICING</a>
-          </li>
-        </ul>
-        <button className="button button_black_border">GET AN INVITE</button>
-      </div>}
-      <div className="overlay"></div>
-    </nav>
+    <>
+      {navOpen && <div className="overlay"></div>}
+      <nav className="nav">
+        <div className="wrapper">
+          <div className="nav_main">
+            <Link to="/" className="logo">
+              <img src={logo} alt="logo and go to home" />
+            </Link>
+            {!isDesktop && <button
+              aria-expanded={navOpen ? "true" : "false"}
+              className="toggle-menu"
+              onClick={handleToggleMenu}
+            >
+              <img
+                src={navOpen ? close : menu}
+                alt={navOpen ? "close menu" : "open menu"}
+              />
+            </button>}
+          </div>
+          {isDesktop && navItems}
+          {navOpen && <div className="nav_list">{navItems}</div>}
+        </div>
+      </nav>
+    </>
   );
 }
 
