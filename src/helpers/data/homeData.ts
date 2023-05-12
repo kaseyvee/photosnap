16bd3e883ddeeb51d4314ac00f19faa9e9540250
kethnal <kaseyvaldez@hotmail.com>
2023-05-12 03:32:42 -0700

    Create home data

---
+++ src/helpers/data/homeData.ts
@@ -0,0 +1,66 @@
+const homeData = {
+  actionCalls: [
+    {
+      title: "CREATE AND SHARE YOUR PHOTO STORIES.",
+      description: "Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others.",
+      image: "create-and-share.jpg",
+      buttonText: "GET AN INVITE",
+      cardColour: "black",
+    },
+    {
+      title: "BEAUTIFUL STORIES EVERY TIME",
+      description: "We provide design templates to ensure your stories look terrific. Easily add photos, text, embed maps and media from other networks. Then share your story with everyone.",
+      image: "beautiful-stories.jpg",
+      buttonText: "VIEW THE STORIES",
+      cardColour: "black",
+    },
+    {
+      title: "DESIGNED FOR EVERYONE",
+      description: "Photosnap can help you create stories that resonate with your audience.  Our tool is designed for photographers of all levels, brands, businesses you name it. ",
+      image: "designed-for-everyone.jpg",
+      buttonText: "GET AN INVITE",
+      cardColour: "black",
+    }
+  ],
+  stories: [
+    {
+      title: "The Mountains",
+      author: "John Appleseed",
+      image: "mountains.jpg"
+    },
+    {
+      title: "Sunset Cityscapes",
+      author: "Benjamin Cruz",
+      image: "sunset-cityscapes.jpg"
+    },
+    {
+      title: "18 Days Voyage",
+      author: "Alexei Borodin",
+      image: "18-days-voyage.jpg"
+    },
+    {
+      title: "Architecturals",
+      author: "Samantha Brooke",
+      image: "architecturals.jpg"
+    },
+  ],
+  offers: [
+    {
+      title: "100% Responsive",
+      description: "No matter which the device you’re on, our site is fully responsive and stories look beautiful on any screen.",
+      image: "responsive.svg"
+    },
+    {
+      title: "No Photo Upload Limit",
+      description: "Our tool has no limits on uploads or bandwidth. Freely upload in bulk and share all of your stories in one go.",
+      image: "no-limit.svg"
+    },
+    {
+      title: "Available to Embed",
+      description: "Embed Tweets, Facebook posts, Instagram media, Vimeo or YouTube videos, Google Maps, and more. ",
+      image: "embed.svg"
+    }
+  ]
+};
+
+export default homeData;
