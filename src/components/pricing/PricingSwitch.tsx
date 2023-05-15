596ce55dba8727a4ab16d639ea0aab839bce82db
kethnal <kaseyvaldez@hotmail.com>
2023-05-15 11:17:42 -0700

    Add pricing switch logic

---
+++ src/components/pricing/PricingSwitch.tsx
@@ -0,0 +1,23 @@
+interface PricingSwitchProps {
+  subscriptionLength: string;
+  handleToggleSubscription: (length: string | null) => void;
+}
+
+function PricingSwitch({ subscriptionLength, handleToggleSubscription }: PricingSwitchProps) {
+
+  return (
+    <div className={`pricing-switch ${subscriptionLength} subscription-length`}>
+      <span className="month" onClick={() => handleToggleSubscription("monthly")}>Monthly</span>
+      <button
+        className='pricing-switch_button'
+        onClick={() => handleToggleSubscription(null)}
+        aria-label={`click to show ${subscriptionLength} pricing`}
+      >
+        <span className='pricing-switch_button_toggle'></span>
+      </button>
+      <span className="year" onClick={() => handleToggleSubscription("yearly")}>Yearly</span>
+    </div>
+  );
+}
+
+export default PricingSwitch;
--- src/pages/_pricing.tsx
+++ src/pages/_pricing.tsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import InviteBanner from "../components/InviteBanner";
 import PricingCard from "../components/pricing/PricingCard";
+import PricingSwitch from "../components/pricing/PricingSwitch";
 import TitleCard from "../components/TitleCard";
 import pricingData from "../helpers/data/pricingData";
 
 function Pricing() {
   const [subscriptionLength, setSubscriptionLength] = useState("monthly");
+
+  function handleToggleSubscription(length: string | null) {
+    if (!length) return setSubscriptionLength(subscriptionLength === "monthly" ? "yearly" : "monthly");
+    return setSubscriptionLength(length);
+  }
 
   const pricingList = pricingData.pricing.map(pricing => {
     return (
@@ -29,9 +35,10 @@ function Pricing() {
       />
       <div className="pricing_data">
         <div className="pricing_data_options">
-          <div className="pricing_data_options_switch">
-
-          </div>
+          <PricingSwitch
+            subscriptionLength={subscriptionLength}
+            handleToggleSubscription={handleToggleSubscription}
+          />
           <ul>
             {pricingList}
           </ul>
